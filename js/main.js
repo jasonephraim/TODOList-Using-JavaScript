c115c1ff12b598f3b81ad0a45b9701747fb4de94
Jason Ephraim <jason.ephraim.novatex@gmail.com>
2020-04-22 11:35:06 -0500

    added function to move completed tasks

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@
 let taskInput = document.getElementById("new-task"); //Add a new task.
 let addTaskButton = document.getElementsByTagName("button")[0]; //first button
 let incompleteTaskHolder = document.getElementById("incomplete-tasks"); //ul of #incomplete-tasks
-let completedTasksHolder = document.getElementById("completed-tasks"); //completed-tasks
+let completedTaskHolder = document.getElementById("completed-tasks"); //completed-tasks
+let completeTaskCheckbox = document.querySelector("input[id=task-checkbox]"); //task completed check box
+let deleteButton = document.getElementById("deleteButton");
 
 // click add task button enters new task
 
@@ -11,12 +13,26 @@ addTaskButton.addEventListener("click", function () {
   event.preventDefault();
   let taskTitle = taskInput.value;
   let liItem = document.createElement("li");
-  liItem.innerHTML =
-    "<li id='" +
-    taskTitle +
-    "'><form class='form-inline'><div class='form-group mb-2'><input type='checkbox' class='form-check-input' /><label>" +
-    taskTitle +
-    "</label><button class='btn btn-danger delete'>Delete</button></div></form></li>";
+  liItem.innerHTML = `<li id='${taskTitle}-li'><form class='form-inline'><div class='form-group mb-2'><input type='checkbox' id='task-checkbox' class='form-check-input' /><label>${taskTitle}</label><button id='deleteButton' class='btn btn-danger delete' onclick="getData()">Delete</button></div></form></li>`;
 
   incompleteTaskHolder.appendChild(liItem);
+});
+
+// check box moves task to corrisponding section
+
+completeTaskCheckbox.addEventListener("change", function () {
+  let item = $(this).closest("li");
+  if (this.checked) {
+    item.appendTo(completedTaskHolder);
+  } else {
+    item.appendTo(incompleteTaskHolder);
+  }
+});
+
+// delete task when corrisponding delete button is clicked
+
+deleteButton.addEventListener("click", function () {
+  event.preventDefault();
+  let item = $(this).closest("li");
+  item.remove();
 });
