1c5329d865a1a823d8edb8e858c4f57c1b087209
Jason Ephraim <jason.ephraim.novatex@gmail.com>
2020-04-21 21:24:27 -0500

    init commit for todo app

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// declaring global variables from add task html fields and ul elements
+
+let taskInput = document.getElementById("new-task"); //Add a new task.
+let addTaskButton = document.getElementsByTagName("button")[0]; //first button
+let incompleteTaskHolder = document.getElementById("incomplete-tasks"); //ul of #incomplete-tasks
+let completedTasksHolder = document.getElementById("completed-tasks"); //completed-tasks
+
+// click add task button enters new task
+
+addTaskButton.addEventListener("click", function () {
+  let taskTitle = taskInput.value;
+  let liItem = document.createElement("li");
+  liItem.innerHTML =
+    "<li><form class='form-inline'><div class='form-group mb-2'><input type='checkbox' class='form-check-input' /><label>" +
+    taskTitle.value +
+    "</label><button class='btn btn-danger delete'>Delete</button></div></form></li>";
+
+  incompleteTaskHolder.appendChild(liItem);
+});
